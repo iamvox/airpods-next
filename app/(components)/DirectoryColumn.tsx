01ba99809e22208200d684bde411bae0f6e6990f
vox <drewdebelo@gmail.com>
2022-12-17 14:18:20 +0000

    minor fixes

--- app/(components)/DirectoryColumn.tsx
+++ app/(components)/DirectoryColumn.tsx
@@ -16,9 +16,9 @@ const DirectoryColumn = ({ column }: Column) => {
       {column.map((section) => (
         <div
           key={section.title}
-          className="relative flex w-full flex-col overflow-hidden border-b-[1px]  font-sans  text-[12px] text-[#000000]   md:border-0 md:pt-[24px]"
+          className="relative flex w-full flex-col overflow-hidden border-b-[1px]  font-sans  text-[12px] text-[#000000]   md:border-0 md:pt-[24px] "
         >
-          <h3 className="z-[10] flex flex-row justify-between overflow-hidden whitespace-pre bg-[#f5f5f7] py-[10px] md:mb-[9.6px] md:mr-[20px] md:py-0 md:font-semibold">
+          <h3 className="z-[10] flex flex-row justify-between overflow-hidden whitespace-pre bg-[#f5f5f7] py-[10px] md:mb-[9.6px] md:mr-[20px] md:py-0 md:font-semibold ">
             {section.title}
             <figure
               onClick={() => setActive(!active)}
@@ -39,7 +39,7 @@ const DirectoryColumn = ({ column }: Column) => {
             {section.subtitles?.map((title: any) => (
               <li
                 key={uuid()}
-                className={`-z-[300] flex w-full basis-1/5 flex-col py-[6px] px-[14px] hover:underline md:mb-[9.6px] md:block md:p-0 `}
+                className={`-z-[300] flex w-full basis-1/5 cursor-pointer flex-col py-[6px] px-[14px] hover:underline md:mb-[9.6px] md:block md:p-0 `}
               >
                 {title}
               </li>
--- app/(sections)/Footer.tsx
+++ app/(sections)/Footer.tsx
@@ -4,9 +4,15 @@ const Footer = () => {
   return (
     <div className="mx-auto w-full max-w-[980px] pt-[17px] pb-[19px] text-[12px] text-[#6e6e73] ">
       <div className="mb-[7px] border-[#d2d2d7] pb-[8px] md:border-b-[1px]">
-        More ways to shop: <a>Find an Apple Store</a> or{" "}
-        <a className="text-[#06c]">other retailer</a> near you. Or call
-        1-800-MY-APPLE.
+        More ways to shop:{" "}
+        <a className="cursor-pointer text-[#06c] hover:underline">
+          Find an Apple Store
+        </a>{" "}
+        or{" "}
+        <a className="cursor-pointer text-[#06c] hover:underline">
+          other retailer
+        </a>{" "}
+        near you. Or call 1-800-MY-APPLE.
       </div>
       <div className="mt-[5px] pt-[5px] text-[12px] text-[#6e6e73] md:hidden">
         United States
@@ -15,34 +21,34 @@ const Footer = () => {
         <div className=" mr-[5px] text-[12px] text-[#6e6e73] ">
           Copyright © 2022 Apple Inc. All rights reserved.
         </div>
-        <div className="relative -top-[5px] z-[1] mt-[5px] mr-[5px] overflow-hidden">
+        <div className="relative -top-[5px] z-[1] mt-[5px] mr-[5px] overflow-hidden ">
           <a
             href="/"
-            className="mr-[7px] mt-[5px] inline-block whitespace-nowrap border-r-[1px] border-[#d2d2d7] pr-[10px]"
+            className="mr-[7px] mt-[5px] inline-block cursor-pointer whitespace-nowrap border-r-[1px] border-[#d2d2d7] pr-[10px] hover:underline"
           >
             Privacy Policy
           </a>
           <a
             href="/"
-            className="mr-[7px] mt-[5px]  inline-block whitespace-nowrap border-r-[1px] border-[#d2d2d7] pr-[10px]"
+            className="mr-[7px] mt-[5px]  inline-block cursor-pointer whitespace-nowrap border-r-[1px] border-[#d2d2d7] pr-[10px] hover:underline"
           >
             Terms of Use
           </a>
           <a
             href="/"
-            className="mr-[7px] mt-[5px] inline-block whitespace-nowrap border-r-[1px] border-[#d2d2d7] pr-[10px]"
+            className="mr-[7px] mt-[5px] inline-block cursor-pointer whitespace-nowrap border-r-[1px] border-[#d2d2d7] pr-[10px] hover:underline"
           >
             Sales and Refunds
           </a>
           <a
             href="/"
-            className="mr-[7px] mt-[5px] inline-block whitespace-nowrap border-r-[1px] border-[#d2d2d7] pr-[10px]"
+            className="mr-[7px] mt-[5px] inline-block cursor-pointer whitespace-nowrap border-r-[1px] border-[#d2d2d7] pr-[10px] hover:underline"
           >
             Legal
           </a>
           <a
             href="/"
-            className="mr-[7px] mt-[5px] inline-block whitespace-nowrap  pr-[10px]"
+            className="mr-[7px] mt-[5px] inline-block cursor-pointer whitespace-nowrap pr-[10px]  hover:underline"
           >
             Site map
           </a>
